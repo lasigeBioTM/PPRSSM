db31c0169c819fc5822be852fa370b41ce0f1b49
Pedro Ruas <p_s_ruas@hotmail.com>
2019-10-07 15:10:33 +0100

    script get_data.sh changed

--- get_data.sh
+++ get_data.sh
@@ -5,3 +5,5 @@ wget ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.obo
 wget http://labs.rd.ciencias.ulisboa.pt/dishin/hp.db
 wget http://labs.rd.ciencias.ulisboa.pt/dishin/chebi.db
 wget http://purl.obolibrary.org/obo/hp.obo
+wget ftp://ftp.geneontology.org/go/ontology/go-basic.obo
+wget http://labs.rd.ciencias.ulisboa.pt/dishin/go.db
